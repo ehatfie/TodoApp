a157b95aaea9ee3597bcc8f45433397c4841d3e7
Erik Hatfield <ehhatfield@protonmail.com>
2020-11-18 18:48:06 -0600

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Erik Hatfield on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
