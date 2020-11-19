47193572123e24280457e0a1e22eb3bb9210efb7
Erik Hatfield <ehhatfield@protonmail.com>
2020-11-19 12:04:15 -0600

    initial

---
+++ TodoApp/APICaller.swift
@@ -0,0 +1,50 @@
+//
+//  APICaller.swift
+//  TodoApp
+//
+//  Created by Erik Hatfield on 11/18/20.
+//
+
+import Foundation
+
+class DecodedObject: Decodable, Identifiable {
+    let id: String
+    let title: String
+    let completed: Bool
+}
+
+class DecodedObjectOuter: Decodable {
+    let data: [DecodedObject]
+}
+
+class APICaller {
+    private var urlString: String = "http://localhost:8080/todos/"
+    
+    static var shared: APICaller = APICaller()
+    
+    private init() {}
+    
+    func fetchData(completion: @escaping ([DecodedObject]) -> Void) {
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { data, res, err in
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    
+                    print("data", data)
+                    print("res", res)
+                    print("err", err)
+                    if let string = data as? String {
+                        print("STRING \(string)")
+                    }
+                    
+                    if let json = try? decoder.decode(DecodedObjectOuter.self, from: data) {
+                        print("GOTTEM \(json)")
+                        completion(json.data)
+                    } else {
+                        print("NO GOTTEM")
+                    }
+                }
+            }.resume()
+        }
+    }
+}
--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -12,24 +12,72 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.title, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    private var dataItems: [DecodedObject] = []
 
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        VStack {
+            Button("BUTTON TITLE", action: fetchData)
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        Text(item.title!)
+                        Text("is completed: \(getIsCompleted(isCompleted: item.completed))")
+                    }
+                    
+                }
+                .onDelete(perform: deleteItems)
+//                ForEach(dataItems) { item in
+//                    Text("Item at \(item, formatter: itemFormatter)")
+//                }
+//                .onDelete(perform: deleteItems)
             }
-            .onDelete(perform: deleteItems)
+            .toolbar {
+                #if os(iOS)
+                EditButton()
+                #endif
+                
+                Button(action: addItem) {
+                    Label("Add Item", systemImage: "plus")
+                }
+            }
         }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
+        
+        
+    }
+    private func getIsCompleted(isCompleted: Bool) -> String {
+        return isCompleted ? "TRUE" : "False"
+    }
+    private func fetchData() {
+        
+        APICaller.shared.fetchData { data in
+            //self.dataItems = data.data
+            print("received \(data.count) values")
+            self.addResult(data: data)
+        }
+    }
+    
+    private func search() {
+        
+    }
+    
+    private func addResult(data: [DecodedObject]) {
+        for value in data {
+            withAnimation {
+                let newItem = Item(context: viewContext)
+                newItem.title = value.title
+                newItem.completed = value.completed
+                do {
+                    try viewContext.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
 
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
             }
         }
     }
@@ -37,7 +85,7 @@ struct ContentView: View {
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            newItem.title = "title"
 
             do {
                 try viewContext.save()
@@ -75,6 +123,9 @@ private let itemFormatter: DateFormatter = {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        Group {
+            ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        }
     }
 }
