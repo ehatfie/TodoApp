140cbc5d9573dc9e85a4150d86e3e7f359c17145
Erik Hatfield <ehhatfield@protonmail.com>
2022-05-12 20:45:18 -0500

    inprog

---
+++ TodoApp/Views/ByDateView.swift
@@ -0,0 +1,20 @@
+//
+//  ByDateView.swift
+//  TodoApp
+//
+//  Created by Erik Hatfield on 5/12/22.
+//
+
+import SwiftUI
+
+struct ByDateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ByDateView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByDateView()
+    }
+}
